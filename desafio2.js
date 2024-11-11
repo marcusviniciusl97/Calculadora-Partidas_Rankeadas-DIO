180f6109901d7758c50881115d08de072f2338d2
Marcus Vinicius <marcusviniciusrocha97@gmail.com>
2024-11-11 15:33:24 -0300

    Entrega do Desafio

---
+++ desafio2.js
@@ -0,0 +1,52 @@
+let match = ranked(150, 37)// number A is Winners, number B is Loses!!
+
+//console.log("Your ranked classification is " + match)
+
+function ranked(numA, numB){
+    let calculation = numA - numB
+    return calculation
+}
+
+if (match <= 10){
+    function ranked(numA, numB){
+        let calculation = numA - numB
+        return calculation
+    }
+    console.log("Seu Herói tem de " + match + " de saldo Vitórias e está no nível Ferro!");
+} else if (match <= 20 ){
+    function ranked(numA, numB){
+        let calculation = numA - numB
+        return calculation
+    }
+    console.log("Seu Herói tem de " + match + " de saldo Vitórias e está no nível Bronze!");
+} else if (match <= 50 ){
+    function ranked(numA, numB){
+        let calculation = numA - numB
+        return calculation
+    }
+    console.log("Seu Herói tem de " + match + " de saldo Vitórias e está no nível Prata!");
+} else if (match <= 80 ){
+    function ranked(numA, numB){
+        let calculation = numA - numB
+        return calculation
+    }
+    console.log("Seu Herói tem de " + match + " de saldo Vitórias e está no nível Ouro!");
+} else if (match <= 90 ){
+    function ranked(numA, numB){
+        let calculation = numA - numB
+        return calculation
+    }
+    console.log("Seu Herói tem de " + match + " de saldo Vitórias e está no nível Diamante!");
+} else if (match <= 100 ){
+    function ranked(numA, numB){
+        let calculation = numA - numB
+        return calculation
+    }
+    console.log("Seu Herói tem de " + match + " de saldo Vitórias e está no nível Lendário!");
+} else if (match >= 101 ){
+    function ranked(numA, numB){
+        let calculation = numA - numB
+        return calculation
+    }
+    console.log("Seu Herói tem de " + match + " de saldo Vitórias e está no nível Imortal!");
+}
